1832cae8e315da240a790765f7e6f1b3b7d7c33d
Kaitlyn Greve <kgreve14@gmail.com>
2022-04-04 17:29:48 -0400

    commit tree 866af1762eaa3fec3131b0188ccac15b13788f80

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideFeet){
   // Write your code here!
+  let result
+  if(rideFeet <= 400) {
+    result = 'This one is on me!';
+  }else if(rideFeet > 400 && rideFeet <= 2000) {
+    result = 'That will be twenty bucks.';
+  }else if(rideFeet > 2000 && rideFeet <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  }else if(rideFeet > 2500) {
+    result = 'No can do.';
+  }
+
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result = (city == 'NYC') ? 'Ok, sounds good.' : 'No go.';
+  
+
+  return result
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let result
+  switch(result) {
+    case tip == 'generous':
+      console.log('Thank you so much.');
+  }
 }
